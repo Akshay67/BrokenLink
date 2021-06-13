cc06c977097524ecb5055cd43a75594415127b33
Akshay Shivekar <akshayshivekar06081995@gmail.com>
2021-06-13 09:23:31 +0530

    colorma package added

--- findBrokenLink_without_bs4.py
+++ findBrokenLink_without_bs4.py
@@ -5,16 +5,16 @@ import colorama
 from colorama import Fore, Back, Style
 
 colorama.init(autoreset=True)
-r = requests.get('https://bugcrowd.com/programs')
+r = requests.get('https://www.google.com/')
 
 
 doc = etree.HTML(r.content)
 for url in doc.xpath('//a[@href]'):
     #print(url.get('href'))
     if r.status_code == 200:
-        print(url.get('href'), Back.GREEN +' 200 OK!')
+        print(url.get('href'), Back.GREEN +' \t 200 OK!')
     elif r.status_code == 404:
-        print(url.get('href'), Back.YELLOW + ' 404 Not Found! ')
+        print(url.get('href'), Back.YELLOW + ' \t 404 Not Found! ')
     elif r.status_code == 500:
         print(url.get('href'), Back.RED + ' - Yooo Man We Got 500 -->  Need Backup..!')
     #print( requests.get(url.get('href')))
